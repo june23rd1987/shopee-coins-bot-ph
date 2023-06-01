f85d481a86af4047032b78e81713a00732b3eef9
june23rd1987 <june23rd1987@gmail.com>
2023-06-01 10:53:37 +0800

    Update text.ts

--- src/text.ts
+++ src/text.ts
@@ -1,13 +1,13 @@
-export const COIN_RECEIVED = '明天再回來'
-export const FAILURE = '很抱歉，您的身份驗證已遭到拒絕。'
-export const PLAY_PUZZLE = '點擊以重新載入頁面'
-export const RECEIVE_COIN = '完成簽到'
-export const SHOPEE_REWARD = '蝦幣獎勵'
-export const TOO_MUCH_TRY = '您已達到今日驗證次數上限。'
-export const USE_LINK = '使用連結驗證'
-export const USE_EMAIL_LINK = '透過電子郵件連結驗證'
+export const COIN_RECEIVED = 'COIN RECEIVED'
+export const FAILURE = 'FAILED'
+export const PLAY_PUZZLE = 'PLAY PUZZLE'
+export const RECEIVE_COIN = 'RECEIVE COIN'
+export const SHOPEE_REWARD = 'SHOPEE REWARD'
+export const TOO_MUCH_TRY = 'TOO MUCH TRY'
+export const USE_LINK = 'USE LINK'
+export const USE_EMAIL_LINK = 'USE EMAIL LINK'
 export const WRONG_PASSWORDS = [
-  '你的帳號或密碼不正確，請再試一次',
-  '登入失敗，請稍後再試或使用其他登入方法',
-  '您輸入的帳號或密碼不正確，若遇到困難，請重設您的密碼。'
+  'WRONG PASSWORD 1',
+  'WRONG PASSWORD 2',
+  'WRONG PASSWORD 3'
 ]
